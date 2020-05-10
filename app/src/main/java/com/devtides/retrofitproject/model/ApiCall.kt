3fb6d83ccd1e04e838e05bcdb05f27362f10d298
VivekSharma811 <vs811861@gmail.com>
2020-05-10 20:44:36 +0530

    Retrofit API added

---
+++ app/src/main/java/com/devtides/retrofitproject/model/ApiCall.kt
@@ -0,0 +1,11 @@
+package com.devtides.retrofitproject.model
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiCall {
+
+    @GET("apiCall")
+    fun callGet() : Call<ApiCallResponse>
+
+}
